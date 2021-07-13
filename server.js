85a4435e1327ed57eb65bc1ffc22d229506fdf09
Gabri3el <g_gabrielmerino@yahoo.com.br>
2021-07-13 15:54:07 -0300

    changes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+var history = require('connect-history-api-fallback');
 
 var app = express()
+
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
